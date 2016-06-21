13b6d9238a38cb70655697fe402ce2818fc60d7e
bananadagua <pedrohbshbs@gmail.com>
2016-06-21 14:37:05 -0300

    adicionado exercicio do menino PH sobre list

---
+++ Exercício Vetor 21.06.2016/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Carro
+    {
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Carro> carros = new List<Carro>();
+            for (int i = 0; i < n; i++)
+			{
+                carros.Add(new Carro());
+            }
+        }
+    }
+}
